16950624587351d74b3dae62238a7de33a226bf3
Max <87244960+kabachok78@users.noreply.github.com>
2021-07-16 12:09:10 +0200

    Add files via upload

---
+++ mario.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int n;
+
+    do
+    {
+        n = get_int("Height: \n");
+    }
+    while (n<1 || n >8);
+
+    for(int i = 0; i<n; i++)
+    {
+        for(int j = n-1; j>i; j--)
+        {
+            printf(" ");
+        }
+
+        for(int k = 0; k<=i; k++)
+        {
+            printf("#");
+        }
+
+        printf("  ");
+
+        for(int l = 0; l<=i; l++)
+        {
+            printf("#");
+        }
+        
+
+        printf("\n");
+
+
+    }
+}
